f5186a50f3aa0ffddf4933ce4a315ea6f309017e
Miguel Del Moral <mikedm195@gmail.com>
2016-05-04 15:15:07 -0500

    mesa

--- mesa.h
+++ mesa.h
@@ -43,29 +43,85 @@ void Mesa::dibujar(double x,double y, double z){
 
         setColor(100.0, 54.0, 2.0);//Colorea las paredes de la mesa (cafe)
         
-        glTranslatef ( 0, .7, 0);//traslada la extremidad
-    
+        glTranslatef ( 0, .2, 0);//traslada la extremidad
+   		//========Dibuja lado derecho de la mesa==============// 
         glPushMatrix(); 
-            glTranslatef ( 0, 0, 5.5);//traslada la extremidad
-            glScalef ( 22, 2.0, .5);//Escala la extremidad
+            glTranslatef ( 5.5, 0, 6.3075);//traslada la extremidad
+            glScalef ( 9, 2.0, 2);//Escala la extremidad
             glutSolidCube (1.0);//Dibuja
         glPopMatrix();
-        glPushMatrix(); 
-            glTranslatef ( 0, 0, -5.5);//traslada la extremidad
-            glScalef ( 22, 2.0, .5);//Escala la extremidad
+		glPushMatrix(); 
+            glTranslatef ( -5.5, 0, 6.3075);//traslada la extremidad
+            glScalef ( 9, 2.0, 2);//Escala la extremidad
             glutSolidCube (1.0);//Dibuja
         glPopMatrix();
-        glPushMatrix(); 
-            glTranslatef ( 11, 0, 0);//traslada la extremidad
-        glScalef ( .5, 2.0, 11);//Escala la extremidad
-        glutSolidCube (1.0);//Dibuja
-        glPopMatrix();
-        glPushMatrix(); 
-            glTranslatef ( -11, 0, 0);//traslada la extremidad
-            glScalef ( .5, 2.0, 11);//Escala la extremidad
+		glPushMatrix(); 
+            glTranslatef ( 0, 0, 7);//traslada la extremidad
+            glScalef ( 26, 2.0, 2);//Escala la extremidad
             glutSolidCube (1.0);//Dibuja
         glPopMatrix();
-		
+   		//========Dibuja lado izquierdo de la mesa==============// 
+        glPushMatrix(); 
+            glTranslatef ( 5.5, 0, -6.3075);//traslada la extremidad
+            glScalef ( 9, 2.0, 2);//Escala la extremidad
+            glutSolidCube (1.0);//Dibuja
+        glPopMatrix();
+		glPushMatrix(); 
+            glTranslatef ( -5.5, 0, -6.3075);//traslada la extremidad
+            glScalef ( 9, 2.0, 2);//Escala la extremidad
+            glutSolidCube (1.0);//Dibuja
+        glPopMatrix();
+		glPushMatrix(); 
+            glTranslatef ( 0, 0, -7);//traslada la extremidad
+            glScalef ( 26, 2.0, 2);//Escala la extremidad
+            glutSolidCube (1.0);//Dibuja
+        glPopMatrix();	
+   		//========Dibuja lado de arriba de la mesa==============// 
+        glPushMatrix(); 
+            glTranslatef ( 11.8075, 0, 0);//traslada la extremidad
+	        glScalef ( 2, 2.0, 9);//Escala la extremidad
+    	    glutSolidCube (1.0);//Dibuja
+        glPopMatrix();
+        glPushMatrix(); 
+            glTranslatef ( 12.8075, 0, 0);//traslada la extremidad
+	        glScalef ( 1.5, 2.0, 16);//Escala la extremidad
+    	    glutSolidCube (1.0);//Dibuja
+        glPopMatrix();
+		//========Dibuja lado de abajo de la mesa==============// 
+        glPushMatrix(); 
+            glTranslatef ( -11.8075, 0, 0);//traslada la extremidad
+	        glScalef ( 2, 2.0, 9);//Escala la extremidad
+    	    glutSolidCube (1.0);//Dibuja
+        glPopMatrix();
+        glPushMatrix(); 
+            glTranslatef ( -12.8075, 0, 0);//traslada la extremidad
+	        glScalef ( 1.5, 2.0, 16);//Escala la extremidad
+    	    glutSolidCube (1.0);//Dibuja
+        glPopMatrix();
+/*
+        glPushMatrix(); 
+            glTranslatef ( -11.8075, 0, 0);//traslada la extremidad
+            glScalef ( 2, 2.0, 9);//Escala la extremidad
+            glutSolidCube (1.0);//Dibuja
+        glPopMatrix();
+*/	
+		setColor(0,0,0);
+		glPushMatrix(); 
+            glTranslatef ( -11, 0, 5.5);//traslada la extremidad
+            glutSolidSphere (0.615,20,20);//Dibuja
+			glTranslatef ( 11, 0, 0);//traslada la extremidad
+            glutSolidSphere (0.615,20,20);//Dibuja
+			glTranslatef ( 11, 0, 0);//traslada la extremidad
+            glutSolidSphere (0.615,20,20);//Dibuja
+			glTranslatef ( 0, 0, -11);//traslada la extremidad
+            glutSolidSphere (0.615,20,20);//Dibuja
+			glTranslatef ( -11, 0, 0);//traslada la extremidad
+            glutSolidSphere (0.615,20,20);//Dibuja
+			glTranslatef ( -11, 0, 0);//traslada la extremidad
+            glutSolidSphere (0.615,20,20);//Dibuja
+        glPopMatrix();
+
+
 		setColor(255,255,255);
     glPopMatrix();
 }
